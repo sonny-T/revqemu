21217a0610f33763bfd4b149212173ef1b0839f8
sonnylier <sonny_lier@163.com>
2021-12-09 23:27:40 +0800

    isdecodeblock for single instr

--- linux-user/ptc.c
+++ linux-user/ptc.c
@@ -1310,6 +1310,8 @@ int64_t ptc_isdecodeblock(uint64_t virtual_address){
 
     if(tb->isIllegal)
       return 0;
+    if(!tb->isDirectJmp && !tb->isDirectcall && (tb->icount==1))
+      return 0;
     return 1;
 }
 
